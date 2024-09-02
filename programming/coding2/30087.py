55c259e868969dc276bef3335482a04cf7bc3c2a
bllor <duki0105@gmail.com>
2024-09-02 22:41:40 +0900

    9.2

---
+++ programming/coding2/30087.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+for i in range(n):
+    str= input()
+    if str=='Algorithm':
+        print('204')
+    elif str == 'DataAnalysis':
+        print('207')
+    elif str == 'ArtificialIntelligence':
+        print('302')
+    elif str == 'CyberSecurity':
+        print('B101')
+    elif str == 'Network':
+        print('303')
+    elif str == 'Startup':
+        print('501')
+    else:
+        print('105')        
