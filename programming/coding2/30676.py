e105686e23a3cdebb0fe7394544d75ef6d789369
bllor <duki0105@gmail.com>
2024-09-23 20:42:56 +0900

    9.23

--- programming/coding2/30676.py
+++ programming/coding2/30676.py
@@ -1,9 +1,16 @@
 v = int(input())
 
-switch(v){
-    case 620<=x and x<=780:
-        print('red')
-        return;
-    default:
-        return;
-}
+if 620<=v and v<=780:
+    print('Red')
+elif 590<=v and v<620:
+    print('Orange')
+elif 570<=v and v<590:
+    print('Yellow')
+elif 495<=v and v<570:
+    print('Green')
+elif 450<=v and v<495:
+    print('Blue')
+elif 425<=v and v<450:
+    print('Indigo')
+else:
+    print('Violet')
