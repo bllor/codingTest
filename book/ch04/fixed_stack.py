60c18e4130514790f3b73ac353dc7dacdca54adf
bllor <duki0105@gmail.com>
2025-01-28 13:05:53 +0900

    1.28

--- book/ch04/fixed_stack.py
+++ book/ch04/fixed_stack.py
@@ -41,6 +41,24 @@ class FixedStack:
     def clear(self)-> None:
         self.ptr = 0
         
-    # def find(self, value:Any)->Any:
-        # for i in range(self.ptr -1, -1, -1):
-            
+    def find(self, value:Any)->Any:
+        for i in range(self.ptr -1, -1, -1):
+            if self.stk[i]==value:
+                return i
+        return -1
+    
+    def count(self, value:Any)->bool:
+        c = 0
+        for i in range(self.ptr):
+            if self.stk[i]==value:
+                c+=1
+        return c
+    
+    def __contains__(self,value:Any)->bool:
+        return self.count(value)
+
+    def dump(self)->None:
+        if self.is_empty():
+            print('no data')
+        else:
+            print(self.stk[:self.ptr]) #0번부터 ptr-1번까지 출력        
