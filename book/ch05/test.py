705467f4f6d19b3de49c0ef2e462fdd5e08a15e3
bllor <duki0105@gmail.com>
2025-02-05 22:00:27 +0900

    2.5

--- book/ch05/test.py
+++ book/ch05/test.py
@@ -1,2 +1,25 @@
-print(8/22)
-print(22/8)
+# print(8/22)
+# print(22/8)
+
+
+pos = [0]*3
+n = 1
+def put()->None:
+    for i in range(3):
+        print(f'{pos[i]:2}',end='')
+    print()
+
+def set(i:int)->None:
+    global n  # 전역 변수 사용 선언
+
+    for j in range(3):
+        print(f'set[{i}][{j}] {n}번')
+        n=n+1
+        pos[i]=j
+        if i==2:
+            put()
+        else:
+            set(i+1)
+
+set(0)
+        
