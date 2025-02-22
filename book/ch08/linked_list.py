4a0009d9c6a9cbc0b553c23080596770f84dce57
bllor <duki0105@gmail.com>
2025-02-22 23:25:06 +0900

    2.22

--- book/ch08/linked_list.py
+++ book/ch08/linked_list.py
@@ -40,8 +40,8 @@ class LinkedList:
         self.no +=1
         
     def add_last(self, data:Any):
-        if self.head is None:
-            self.add_first(data)
+        if self.head is None: #리스트가 비어있으면
+            self.add_first(data) #맨 앞에 노드를 삽입
         else:
             ptr = self.head
             while ptr.next is not None:
@@ -50,7 +50,8 @@ class LinkedList:
             self.no +=1
             
     def remove_first(self)->None:
-        if self.head is not None:
+        #머리노드(가장 첫 번째 노드)를 제거
+        if self.head is not None: #리스트가 비어 있으면
             self.head = self.current = self.head.next
         self.no -=1
     
@@ -59,17 +60,19 @@ class LinkedList:
             if self.head.next is None:
                 self.remove_first()
             else:
-                ptr = self.head
-                pre = self.head
+                ptr = self.head #스캔중인 노드
+                pre = self.head #스캔중인 노드의 앞쪽 노드
                 
                 while ptr.next is not None:
                     pre = ptr
                     ptr = ptr.next
-                pre.next = None
+                pre.next = None #pre는 삭제 뒤 꼬리 노드
                 self.current  = pre
                 self.no -=1
     
+    #임의의 노드를 삭제하는 함수
     def remove(self,p:Node)->None:
+        #노드 P를 삭제 
         if self.head is not None:
             if p is self.head:
                 self.remove_first()
@@ -94,9 +97,10 @@ class LinkedList:
         self.current = None
         self.no = 0
     
+    #주목 노드를 한 칸 뒤로 이동
     def next(self)->None:
         if self.current is None or self.current.next is None:
-            return False
+            return False # 이동할 수 없음
         self.current = self.current.next
         return True
 
@@ -113,6 +117,14 @@ class LinkedList:
             print(ptr.data)
             ptr = ptr.next
     
+    
+    '''
+    이터러블 객체는 원소를 1개씩 꺼내는 구조의 객체이다.
+    이터러블 객체를 내장함수인 iter()함수에 인수로 전달하면 그 객체에 대한 이터레이터를 반환합니다.
+    이터레이터는 데이터가 줄지어 늘어선 것을 표현하는 객체 입니다.
+    이터레이터의 next함수를 호출하거나, 내장함수인 next()함수에 반복자를 전달하면 줄지어 늘어선 원소를 순차적으로 꺼냅니다.
+    꺼낼 원소가 없으면 stopIterration예외처리를 합니다.
+    '''
     def __iter__(self)-> LinkedListIterator:
         return LinkedListIterator(self.head)
 
